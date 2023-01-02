8088a4655460314e2a2804ad0c8dc62dcf9b6a8a
LiamBrem <liam.e.brem@gmail.com>
2023-01-02 17:33:43 -0500

    made stock class

--- stock.py
+++ stock.py
@@ -1,10 +1,22 @@
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 class Stock:
-
-    name = None
-    price = None
-
     def __init__(self, symbol, price):
         self.symbol = symbol
         self.price = price
+
+        self.tickerObj = yf.Ticker(symbol)
+
+    def symbolHistory(self):
+        return self.tickerObj.history(period="1mo", interval="1d")
+
+    def drawGraph(self):
+        pass
+
+
+#example = Stock("AAPL", 4747)
+
+#print(example.symbolHistory())
+
+#print(example)
