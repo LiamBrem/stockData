b22fc1149d8a5dff83a2af5fd68a0b2d4846aae6
Liam Brem <50599315+LiamBrem@users.noreply.github.com>
2023-02-08 19:33:45 +0000

    get start,end dates

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import yfinance as yf
+import datetime
 
-def introSequence():
-    pass
+
 
 def graph():
     pass
 
-def getInfo():
-    pass
+def getStartEnd():
+    end = datetime.date.today()
+    start = end.replace(year=end.year-10)
+
+    print(start)
+    print(end)
+
+    return start,end
+
+def getPoints(symbol):
+    x = []
+    y = []
+
+    start,end = getStartEnd()
+
+
+    symbol_ticker = yf.Ticker(symbol)
+
+
+    return x,y
+
+if __name__ == "__main__":
+    print("this program compares any stock to the overall S&P 500")
+    symbol = input("Enter the symbol you want to use: ")
+
+    #xpoints, ypoints = getPoints(symbol)
+
+    getStartEnd()
